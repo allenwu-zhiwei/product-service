120c41a99b1f403766b328cf009ca8c8028da21c
zenmang <305611332@qq.com>
2024-10-14 20:21:06 +0800

    #zengmeng_delete unused exception class

--- src/main/java/com/nusiss/productservice/exception/OrderBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.nusiss.productservice.exception;
-
-public class OrderBusinessException extends BaseException {
-
-    public OrderBusinessException(String msg) {
-        super(msg);
-    }
-
-}
